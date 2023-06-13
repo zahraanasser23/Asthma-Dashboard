b7e775725948d46e797b078bc56faec4aba29d1d
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-06-13 19:29:12 +0300

    Update asthma_dashboard.py

--- asthma_dashboard.py
+++ asthma_dashboard.py
@@ -34,7 +34,7 @@ file_1 = st.file_uploader("asthma-deaths-by-county-1.csv", type="csv")
 file_2 = st.file_uploader("asthma-prevalence-2.csv", type="csv")
 file_3 = st.file_uploader("asthma-ed-visit-rates-lghc-indicator-07-.csv", type="csv")
 
-# Load and process the selected data set
+# Load and process the selected data sets
 if st.button("Load Data Set"):
     if file_1 and file_2 and file_3:
         st.write("Loading data sets...")
@@ -53,48 +53,7 @@ if st.button("Load Data Set"):
         st.subheader("Data Set 3")
         st.dataframe(df_3)
 
+        # Process and visualize the data sets as desired
+        # Add your code here for data analysis and visualization using pandas, plotly, and streamlit
+        # For example, you can call the plot_bar_chart and plot_scatter_plot functions on the loaded data sets
 
-# Function to load data set
-def load_data(file):
-    df = pd.read_csv(file)
-    return df
-
-# Function to plot bar chart
-def plot_bar_chart(df, column):
-    fig = px.bar(df, x=column)
-    st.plotly_chart(fig)
-
-# Function to plot scatter plot
-def plot_scatter_plot(df, x_column, y_column):
-    fig = px.scatter(df, x=x_column, y=y_column, color='Asthma', color_continuous_scale='RdBu')
-    st.plotly_chart(fig)
-
-# Set the app title
-st.title("Asthma Data Set Dashboard")
-
-# Load and process the selected data set
-if st.button("Load Data Set"):
-    if uploaded_files:
-        for file in uploaded_files:
-            st.write(f"Loading {file.name}...")
-            df = load_data(file)
-            st.write(f"{file.name} loaded successfully!")
-
-            # Display the data set
-            st.subheader("Data Set")
-            st.dataframe(df)
-
-            # Show descriptive statistics
-            st.subheader("Descriptive Statistics")
-            st.write(df.describe())
-
-            # Show a bar chart
-            st.subheader("Bar Chart")
-            column_to_plot = st.selectbox("Select a Column for Bar Chart", df.columns)
-            plot_bar_chart(df, column_to_plot)
-
-            # Show a scatter plot
-            st.subheader("Scatter Plot")
-            x_column = st.selectbox("Select X Column for Scatter Plot", df.columns)
-            y_column = st.selectbox("Select Y Column for Scatter Plot", df.columns)
-            plot_scatter_plot(df, x_column, y_column)
