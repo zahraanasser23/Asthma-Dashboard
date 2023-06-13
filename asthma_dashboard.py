f6faced38be185786ec2f026f324fd35fee1ac09
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-06-13 19:04:47 +0300

    Update asthma_dashboard.py

--- asthma_dashboard.py
+++ asthma_dashboard.py
@@ -14,7 +14,7 @@ import plotly.express as px
 # Define the data sets
 data_sets = {
     "Death by Country": "https://github.com/zahraanasser23/Asthma-Dashboard/blob/main/asthma-deaths-by-county-1.csv",
-    "Data Set 2": "https://data.world/chhs/28698f95-0637-44f0-9072-a405d90f3f83",
+    "ED visit rates LGHC": "https://github.com/zahraanasser23/Asthma-Dashboard/blob/main/asthma-ed-visit-rates-lghc-indicator-07-.csv",
     "Data Set 3": "https://data.world/chhs/5ea28f96-7bb6-4c18-9c0d-020484fab181"
 }
 
