a2c3b221f1aee285d393af806af4fbe29839c367
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-06-13 14:03:34 +0300

    Update asthma_.py

--- asthma_.py
+++ asthma_.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1oXjX9aWSpW2z4uLEgeJtJRanOhFS6pzg
 """
 
-pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
