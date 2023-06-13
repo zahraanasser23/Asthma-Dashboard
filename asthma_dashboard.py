dfe9d11d9a646c18829e2480cfacbd29a76d38e5
zahraanasser23 <136422348+zahraanasser23@users.noreply.github.com>
2023-06-13 19:22:27 +0300

    Update asthma_dashboard.py

--- asthma_dashboard.py
+++ asthma_dashboard.py
@@ -30,7 +30,47 @@ def plot_scatter_plot(df, x_column, y_column):
 st.title("Asthma Data Set Dashboard")
 
 # Create file uploader for each data set
-uploaded_files = st.file_uploader("https://github.com/zahraanasser23/Asthma-Dashboard/blob/main/asthma-deaths-by-county-1.csv","https://github.com/zahraanasser23/Asthma-Dashboard/blob/main/asthma-ed-visit-rates-lghc-indicator-07-.csv","https://github.com/zahraanasser23/Asthma-Dashboard/blob/main/asthma-prevalence-2.csv",   accept_multiple_files=True)
+file_1 = st.file_uploader("", type="csv")
+file_2 = st.file_uploader("", type="csv")
+file_3 = st.file_uploader("", type="csv")
+
+# Load and process the selected data set
+if st.button("Load Data Set"):
+    if file_1 and file_2 and file_3:
+        st.write("Loading data sets...")
+        df_1 = load_data(file_1)
+        df_2 = load_data(file_2)
+        df_3 = load_data(file_3)
+        st.write("Data sets loaded successfully!")
+
+        # Display the data sets
+        st.subheader("Data Set 1")
+        st.dataframe(df_1)
+
+        st.subheader("Data Set 2")
+        st.dataframe(df_2)
+
+        st.subheader("Data Set 3")
+        st.dataframe(df_3)
+
+
+# Function to load data set
+def load_data(file):
+    df = pd.read_csv(file)
+    return df
+
+# Function to plot bar chart
+def plot_bar_chart(df, column):
+    fig = px.bar(df, x=column)
+    st.plotly_chart(fig)
+
+# Function to plot scatter plot
+def plot_scatter_plot(df, x_column, y_column):
+    fig = px.scatter(df, x=x_column, y=y_column, color='Asthma', color_continuous_scale='RdBu')
+    st.plotly_chart(fig)
+
+# Set the app title
+st.title("Asthma Data Set Dashboard")
 
 # Load and process the selected data set
 if st.button("Load Data Set"):
